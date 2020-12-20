bbe15a455b674e382988449086d0ed9d16595182
Hasan Moni <62002387+hasan-moni-321@users.noreply.github.com>
2020-12-20 07:54:11 +0600

    Add files via upload

---
+++ dataset.py
@@ -0,0 +1,39 @@
+import config
+import torch
+
+
+class BERTDataset:
+    def __init__(self, reviews, targets, tokenizer, maxlen):
+        self.review = review
+        self.target = target
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+
+    def __len__(self):
+        return len(self.review)
+
+    def __getitem__(self, item):
+        review = str(self.review[item])
+        review = " ".join(review.split())
+        target = self.target[item]
+
+        inputs = self.tokenizer.encode_plus(
+            review,
+            None,
+            add_special_tokens=True,
+            max_length=self.max_len,
+            truncation=True,
+            pad_to_max_length=True,
+        )
+
+        ids = inputs["input_ids"]
+        mask = inputs["attention_mask"]
+        token_type_ids = inputs["token_type_ids"]
+
+        return {
+            "ids": torch.tensor(ids, dtype=torch.long),
+            "mask": torch.tensor(mask, dtype=torch.long),
+            "token_type_ids": torch.tensor(token_type_ids, dtype=torch.long),
+            "targets": torch.tensor(target, dtype=torch.float)
+        }
+
