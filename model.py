1f7c259eac76a0a2caff8a29cb40e0ecb2dcb6e6
Hasan Moni <62002387+hasan-moni-321@users.noreply.github.com>
2020-12-24 19:24:13 +0600

    Add files via upload

---
+++ model.py
@@ -0,0 +1,19 @@
+import torch.nn as nn
+import transformers
+from transformers import AutoTokenizer, AutoModelForMaskedLM
+
+import config
+
+
+class BERTBaseUncased(nn.Module):
+    def __init__(self):
+        super(BERTBaseUncased, self).__init__()
+        self.bert = AutoModelForMaskedLM.from_pretrained(config.bert_model) 
+        self.bert_drop = nn.Dropout(0.3)
+        self.out = nn.Linear(768, 1)
+
+    def forward(self, ids, mask, token_type_ids):
+        _, o2 = self.bert(ids, attention_mask=mask, token_type_ids=token_type_ids)
+        bo = self.bert_drop(o2)
+        output = self.out(bo)
+        return output
