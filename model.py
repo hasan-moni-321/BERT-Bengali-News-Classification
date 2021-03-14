b5e735705e331d50a9aef937df3ccda17c7c98c5
Hasan Moni <62002387+hasan-moni-321@users.noreply.github.com>
2021-03-14 15:19:36 +0600

    Update model.py

--- model.py
+++ model.py
@@ -8,12 +8,12 @@ import config
 class BERTBaseUncased(nn.Module):
     def __init__(self):
         super(BERTBaseUncased, self).__init__()
-        self.bert = AutoModelForMaskedLM.from_pretrained(config.bert_model) 
+        self.bert = BertForMaskedLM.from_pretrained(config.bert_model) 
         self.bert_drop = nn.Dropout(0.3)
-        self.out = nn.Linear(768, 1)
+        self.out = nn.Linear(768, 8)
 
     def forward(self, ids, mask, token_type_ids):
         _, o2 = self.bert(ids, attention_mask=mask, token_type_ids=token_type_ids)
-        bo = self.bert_drop(o2)
+        bo = self.bert_drop(o.2)
         output = self.out(bo)
         return output
